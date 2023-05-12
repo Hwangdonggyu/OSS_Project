65c0beac82f4f74f162fec2567489a2599e8d756
황동규 <hdg2342@naver.com>
2023-05-12 14:13:56 +0900

    2023-05-12 메인 페이지 뼈대,css 재구성

---
+++ js/start.js
@@ -0,0 +1,61 @@
+const apiKey = "02b49f3f0bfb423b6ec8846c057d4ba4";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+const main = document.querySelector('#main');
+const result = document.querySelector('#result');
+
+
+function begin(){
+    main.style.animation="fadeOut 1s";
+    setTimeout(() => {
+        result.style.animation="fadeIn 1s";
+        setTimeout(() => {
+            main.style.display = "none";  //display 꺼줌
+            result.style.display = "block";  //display 켜줌
+        }, 450)
+    }, 450);
+}
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if(response.status == 404){
+        document.querySelector(".error").style.display= "block";
+        document.querySelector(".weather").style.display= "none";
+    } else {
+        var data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        if(data.weather[0].main == "Clouds") {
+            weatherIcon.src = "./image/clouds.png";
+        }
+        else if(data.weather[0].main == "Clear") {
+            weatherIcon.src = "./image/clear.png";
+        }
+        else if(data.weather[0].main == "Rain") {
+            weatherIcon.src = "./image/rain.png";
+        }
+        else if(data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "./image/drizzle.png";
+        }
+        else if(data.weather[0].main == "Mist") {
+            weatherIcon.src = "./image/mist.png";
+        }
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display= "none";
+
+    }
+}
+
+searchBtn.addEventListener("click", ()=>{
+    begin();
+    checkWeather(searchBox.value);
+})  
